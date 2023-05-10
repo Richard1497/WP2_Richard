bde01a6207ea8c64934dd9bbee7773f72d6c6579
Richard1497 <richardshadow337@gmail.com>
2023-05-10 11:15:40 +0700

    TUGAS

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo"Nama saya Richard Christian
+            Saya tinggal di Jakarta
+            Olah raga yang saya sukai adalah Berenang";
+        }
+}
